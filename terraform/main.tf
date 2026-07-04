aa4a98ef9d3b900983956337619815fae5d5f01b
manikanta Routhu <manikantar129@gmail.com>
2026-07-04 18:48:35 +0530

    Jerney project

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,8 +22,8 @@ module "vpc" {
     cidr  = var.vpc_cidr
 
     azs     = local.azs
-    private_subnet = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
-    public_subnet  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
+    private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
+    public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
 
     enable_nat_gateway  =   true
     single_nat_gateway  =   true # Cost-saving for dev; use one per AZ for prod
